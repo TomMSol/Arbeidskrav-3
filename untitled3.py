f40e9ba313663f2479d3abb84973156ea381940e
TomMSol <tom.martin.solbakken@gmail.com>
2025-04-12 22:29:33 +0200

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 12 22:21:33 2025
+
+@author: MidnightAhri
+"""
+
+import pandas as pd
+import numpy as np
+
+#excel fil
+filnavn = 'support_uke_24.xlsx'
+data = pd.read_excel(filnavn)
+
+#  data bank
+u_dag = data.iloc[:, 0].to_numpy()       # Ukedag
+kl_slett = data.iloc[:, 1].to_numpy()    # Klokkeslett
+varighet = data.iloc[:, 2].to_numpy()    # Samtalens varighet
+score = data.iloc[:, 3].to_numpy()       # Tilfredshet
+
+# Printer ut 
+print("Ukedag (u_dag):", u_dag[:5])
+print("Klokkeslett (kl_slett):", kl_slett[:5])
+print("Varighet:", varighet[:5])
+print("Score:", score[:5])
